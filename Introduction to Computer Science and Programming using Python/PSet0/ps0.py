1176a2a37da1dd1f097a868865ec10efecb87958
rqnguyen100 <75060011+rqnguyen100@users.noreply.github.com>
2020-11-25 22:11:33 -0800

    Create ps0.py

---
+++ Introduction to Computer Science and Programming using Python/PSet0/ps0.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Oct 13 22:38:58 2020
+
+@author: rogernguyen
+"""
+import numpy
+
+x = input("Enter a Number:")
+x = int(x)
+y = input("Enter a Number:")
+y = int(y)
+
+print(x**y)
+print(numpy.log2(x))
